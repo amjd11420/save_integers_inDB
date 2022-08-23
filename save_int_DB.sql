35d6a1e0f2a4f1991ec281786424a3450f26ea6f
amjd11420 <108552528+amjd11420@users.noreply.github.com>
2022-08-23 12:28:35 +0300

    Add files via upload

--- save_int_DB.sql
+++ save_int_DB.sql
@@ -1,7 +1,7 @@
 USE [master]
 GO
 
-/****** Object:  Database [integers_db]    Script Date: 25/01/44 12:05:46 م ******/
+/****** Object:  Database [integers_db]    Script Date: 25/01/44 12:26:33 م ******/
 DROP DATABASE [integers_db]
 GO
 
